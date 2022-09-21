daf48359747be9d3ae398a55595ab57c2c850d00
Oussama Rjab <rjabo@ext.essilor.com>
2022-09-21 11:44:08 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Donner une valeur de a :\n");
+    scanf("%d",&a);
+    printf("Hello world! :  a =%d\n",a);
+    return 0;
+}
